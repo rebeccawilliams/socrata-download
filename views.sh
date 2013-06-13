b8f5b9b19dc65d80bf4fce47a11a5d7cee7a2479
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 12:30:27 -0400

    Download views

---
+++ views.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if test -z "$SOCRATA_URL"; then
+  echo You need to set the SOCRATA_URL.
+  exit 1
+fi
+DIR="data/$SOCRATA_URL/views"
+
+mkdir -p "$DIR"
+for viewid in $(cat "data/$SOCRATA_URL/viewids"); do
+  test -e "$DIR/${viewid}" || wget -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json"
+done
