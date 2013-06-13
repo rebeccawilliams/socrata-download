fb86857247932ea459c8210535fec9fbbb705035
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 12:21:43 -0400

    debug search.sh

--- search.sh
+++ search.sh
@@ -8,15 +8,16 @@ fi
 
 download_page() {
   [ -z "$1" ] && echo 'You must specify a page number' && return 1
-  file="socrata/datasets/$1"
+  file="data/$SOCRATA_URL/searches/$1"
   test -f "$file" || wget -O "$file" "https://$SOCRATA_URL/browse?utf8=?&page=$1"
 
   # Error on the last page because the last page does not say 'title="last page"'.
-  grep 'title="last page"' "$file" > /dev/null
+  grep 'title="Last Page"' "$file" > /dev/null
 }
 
 mkdir -p "data/$SOCRATA_URL/searches"
-for page in $(seq 1 1000); do
-  echo Downloading page $page
+page=1
+while true; do
   download_page $page || break
+  page=$(($page + 1))
 done
