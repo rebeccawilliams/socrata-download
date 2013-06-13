f70afb3909aa2f54835fdf26ade9089c755855a7
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 12:53:55 -0400

    typos

---
+++ portals.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+import os
+from urllib2 import urlopen
+from lxml.html import fromstring
+
+# Download.
+html = fromstring(urlopen('http://www.socrata.com/customer-spotlight/').read())
+
+# Get portal urls.
+portals = [unicode(t.strip()) for t in html.xpath('//p[strong[a[img[starts-with(@class, "alignnone size-full wp-image")]]]]/text()')]
+
+# Make a directory for each portal.
+for portal in portals:
+    try:
+        os.makedirs(os.path.join('data', portal))
+    except OSError, e:
+        if e.args[0] != 17:
+            raise
+
