4f8ad729dc0dc12e6de960b2ec0b59a3156543fa
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 12:58:42 -0400

    run on all the portals

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+for path in $(ls -d data/[a-z]*); do
+  (
+    export SOCRATA_URL=$(echo "$path" | cut -d/ -f2)
+    echo $SOCRATA_URL
+  )
+done
---
+++ run_one.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if test -z "$SOCRATA_URL"; then
+  echo You need to set the SOCRATA_URL.
+  exit 1
+fi
+
+./search.sh
+./viewids.py
+./views.sh
+# ./rows.sh
+# ./builddb.py
+# ./upload.sh
