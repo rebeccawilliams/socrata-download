fbd2aaeeb78463d7b05f1cd65e432816262e06d8
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 14:12:20 -0400

    don't download bad portals

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
+# Remove bad portals.
+rm -fR data/{consumerfinance.gov,datakc.org,data.undp.com,ethics.gov,dati.wa.gov,metrochicagodata.com,nyc.gov}
+
+# Download good portals.
 for path in $(ls -d data/[a-z]*); do
   (
     export SOCRATA_URL=$(echo "$path" | cut -d/ -f2)
     echo $SOCRATA_URL
-    ./run_one.sh
+    ./run_one.sh &
   )
 done
