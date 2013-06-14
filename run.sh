73e5a28b7e3074759d0215e206d4e85b9de61b1c
Thomas Levine <occurrence@thomaslevine.com>
2013-06-14 03:53:54 -0400

    handle http errors

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ set -e
 # Remove bad portals.
 rm -fR data/{consumerfinance.gov,datakc.org,data.undp.com,ethics.gov,dati.wa.gov,metrochicagodata.com,nyc.gov,bronx.lehman.cuny.edu}
 
+# Something's weird about this one.
+rm -fR data/data.act.gov.au
+
+# Add portals that aren't listed on the Socrata site.
+mkdir -p data/{data.cityofnewyork.us,opendata.socrata.com}
+
 # Download good portals.
 for path in $(ls -d data/[a-z]*); do
   (
--- views.sh
+++ views.sh
@@ -7,7 +7,20 @@ if test -z "$SOCRATA_URL"; then
 fi
 DIR="data/$SOCRATA_URL/views"
 
+tmp=$(mktemp)
 mkdir -p "$DIR"
+echo $tmp
 for viewid in $(cat "data/$SOCRATA_URL/viewids"); do
-  test -e "$DIR/${viewid}" || wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json"
+  if test -e "$DIR/${viewid}" || wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json" 2> $tmp; then continue
+
+  # Skip on 404
+  elif grep 'ERROR 404: Not Found.' $tmp; then continue
+
+  # Wait on API limit.
+  elif grep 'ERROR 429: 429' $tmp; then
+    sleep 2h
+    # Refactor this.
+    wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json"
+
+  fi 
 done
