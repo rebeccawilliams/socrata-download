a2fc61dd887bc4414a69e862d29d1aa4440ef27f
Thomas Levine <occurrence@thomaslevine.com>
2013-06-20 11:43:49 -0400

    sleeping

--- views.sh
+++ views.sh
@@ -11,16 +11,21 @@ tmp=$(mktemp)
 mkdir -p "$DIR"
 echo $tmp
 for viewid in $(cat "data/$SOCRATA_URL/viewids"); do
-  if test -e "$DIR/${viewid}" || wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json" 2> $tmp; then continue
 
-  # Skip on 404
-  elif grep 'ERROR 404: Not Found.' $tmp; then continue
+  if test -e "$DIR/${viewid}" || wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json" 2> $tmp; then
+    # Sleep if it worked
+    sleep 1s
+    continue
 
-  # Wait on API limit.
+  elif grep 'ERROR 404: Not Found.' $tmp; then
+    # Skip on 404, after sleeping
+    sleep 1s
+    continue
+
   elif grep 'ERROR 429: 429' $tmp; then
-    sleep 2h
-    # Refactor this.
-    wget --no-check-certificate -O "$DIR/${viewid}" "https://$SOCRATA_URL/views/${viewid}.json"
+    # Die on API limit, with a note to try later.
+    echo "You've hit an API limit. Try again later. Bye."
+    break
 
   fi 
 done
