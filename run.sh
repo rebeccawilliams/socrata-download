90bc0d2204242b30b4b6ff588a502adea0d580c1
Thomas Levine <occurrence@thomaslevine.com>
2013-07-03 15:27:33 -0700

    get portals from the status page

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
+# Get portals from the website.
+./portals.py
+
 # Remove bad portals.
-rm -fR data/{consumerfinance.gov,datakc.org,data.undp.com,ethics.gov,dati.wa.gov,metrochicagodata.com,nyc.gov,bronx.lehman.cuny.edu}
+# rm -fR data/\{consumerfinance.gov,datakc.org,data.undp.com,ethics.gov,dati.wa.gov,metrochicagodata.com,nyc.gov,bronx.lehman.cuny.edu\}
 
 # Something's weird about this one.
 rm -fR data/data.act.gov.au
 
 # Add portals that aren't listed on the Socrata site.
-mkdir -p data/{data.cityofnewyork.us,opendata.socrata.com}
+mkdir -p data/opendata.socrata.com
 
 # Download good portals.
 for path in $(ls -d data/[a-z]*); do
