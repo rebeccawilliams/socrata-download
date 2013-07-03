6a15a0b2be6131fe5494cc963a0db5bb91ef4e64
Thomas Levine <occurrence@thomaslevine.com>
2013-07-03 10:25:13 -0700

    clean the portal domain names

--- portals.py
+++ portals.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python2
-import os, json
+import os, json, warnings
 from urllib2 import urlopen
 
 # Download.
 portals = json.load(urlopen('http://status.socrata.com/sites'))
 
+def is_domain(potential_url):
+    return '.' in potential_url and ' ' not in potential_url
+
 # Make a directory for each portal.
 for portal in portals:
-    domain = portal['description']
+    if is_domain(portal['description']):
+        domain = portal['description']
+    elif is_domain(portal['name']):
+        domain = portal['name']
+    elif portal['name'] == 'Socrata':
+        continue
+    else:
+        warnings.warn('Could not find a valid domain for %s, skipping' % portal['name'])
+        continue
+
+    domain = domain.replace('https://', '').replace('http://', '')
+
     try:
         os.makedirs(os.path.join('data', domain))
     except OSError, e:
