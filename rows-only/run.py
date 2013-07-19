41e57cb340888720c96475a843745e125454c25c
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 13:14:52 -0400

    skip finished things

--- rows-only/run.py
+++ rows-only/run.py
@@ -13,9 +13,11 @@ def portal_rows(portal, output_dir = 'rows', input_dir = 'data'):
 
     for viewid_raw in open(os.path.join(input_dir, portal, 'viewids')).readlines():
         viewid = viewid_raw[:-1]
-        url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid)
-        urlretrieve(url, filename = os.path.join(portal_dir, viewid))
-        break
+        output_file = os.path.join(portal_dir, viewid)
+        if not os.path.exists(output_file):
+            url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid)
+            urlretrieve(url, filename = output_file)
+    return 0
 
 import os
 if 'http_proxy' in os.environ:
@@ -23,7 +25,7 @@ if 'http_proxy' in os.environ:
     print 'An http_proxy is set, so I am running in parallel with %d processes.' % nprocesses
     from multiprocessing import Pool
     p = Pool(nprocesses)
-    p.map(portal_rows, os.listdir('data'))
+    print p.map(portal_rows, os.listdir('data'))
 else:
     print 'No http_proxy is set, so I am running in series.'
-    map(portal_rows, os.listdir('data'))
+    print map(portal_rows, os.listdir('data'))
