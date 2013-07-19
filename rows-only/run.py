d1df52dfc988736a92aaa17075aa9f4a276ccdf4
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 11:05:50 -0700

    seems to work

--- rows-only/run.py
+++ rows-only/run.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
-import os
-from urllib import urlretrieve
 
-OUTPUT_DIR = 'rows'
-INPUT_DIR = 'data'
+def portal_rows(portal, output_dir = 'rows', input_dir = 'data'):
+    'Download all of the rows for one portal.'
+    import os
+    from urllib import urlretrieve
 
-def portal_rows(portal):
-    portal_dir = os.path.join(OUTPUT_DIR, portal)
+    portal_dir = os.path.join(output_dir, portal)
     try:
         os.makedirs(portal_dir)
     except OSError:
         pass
 
-    for viewid_raw in open(os.path.join(INPUT_DIR, portal, 'viewids')).readlines():
+    for viewid_raw in open(os.path.join(input_dir, portal, 'viewids')).readlines():
         viewid = viewid_raw[:-1]
         url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid)
         urlretrieve(url, filename = os.path.join(portal_dir, viewid))
         break
 
-if 'http_proxy' not in os.environ:
-    raise UserWarning('You have not set an http_proxy.')
-
-for portal in os.listdir(INPUT_DIR):
-    portal_rows(portal)
+import os
+if 'http_proxy' in os.environ:
+    nprocesses = 5
+    print 'An http_proxy is set, so I am running in parallel with %d processes.' % nprocesses
+    from multiprocessing import Pool
+    p = Pool(nprocesses)
+    p.map(portal_rows, os.listdir('data'))
+else:
+    print 'No http_proxy is set, so I am running in series.'
+    map(portal_rows, os.listdir('data'))
