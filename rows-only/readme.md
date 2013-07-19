92a85189677a53e14d81e0f76afcbd2db07782c8
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 10:37:15 -0700

    errors

--- rows-only/readme.md
+++ rows-only/readme.md
@@ -1,2 +1,23 @@
 This directory contains an encapsulated downloader for just the rows,
 assuming we already have lists of viewids.
+
+## Errors
+I'm downloading all error messages because that will allow us to figure
+out which things are federating from where. This first sort of error message
+means that the data are federated to this table rather than natively on
+this portal.
+    
+    {
+      "code" : "not_found",
+      "error" : true,
+      "message" : "Cannot find view with id bxfh-jivs"
+    }
+
+This second sort means that the data are not a table; they might be an
+external dataset.
+
+    {
+      "code" : "invalid_request",
+      "error" : true,
+      "message" : "Can only get rows for tabular datasets."
+    }
--- rows-only/run.py
+++ rows-only/run.py
@@ -3,10 +3,19 @@ import os
 from urllib import urlretrieve
 
 OUTPUT_DIR = 'rows'
-HTTP_PROXY = os.environ['http_proxy']
+INPUT_DIR = 'data'
 
-for portal in os.listdir('data'):
-    for viewid in open(os.path.join('data', portal, 'viewids')).readlines():
-        url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid.strip())
-        print url
+try:
+    os.mkdir(OUTPUT_DIR)
+except OSError:
+    pass
+
+for portal in os.listdir(INPUT_DIR):
+    for viewid_raw in open(os.path.join(INPUT_DIR, portal, 'viewids')).readlines():
+        viewid = viewid_raw[:-1]
+        portal = 'explore.data.gov'
+        viewid = 'bxfh-jivs'
+        url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid)
+        urlretrieve(url, filename = os.path.join(OUTPUT_DIR, viewid))
+        break
     break
