5d4adea29ef10e1c6f26cbd55f6308cfbb1b2aae
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 10:16:57 -0700

    iterate

--- rows-only/run.py
+++ rows-only/run.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 import os
+
+from requests import get
 
 for portal in os.listdir('data'):
     for viewid in open(os.path.join('data', portal, 'viewids')).readlines():
-        print portal, viewid
+        url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid.strip())
+        print url
     break
