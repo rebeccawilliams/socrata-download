29fb8a05cc2c6e0d5c9f4d76d3896f766106ad84
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 10:41:10 -0700

    start parallel stuff

--- rows-only/run.py
+++ rows-only/run.py
@@ -5,17 +5,21 @@ from urllib import urlretrieve
 OUTPUT_DIR = 'rows'
 INPUT_DIR = 'data'
 
-try:
-    os.mkdir(OUTPUT_DIR)
-except OSError:
-    pass
+def portal_rows(portal):
+    portal_dir = os.path.join(OUTPUT_DIR, portal)
+    try:
+        os.makedirs(portal_dir)
+    except OSError:
+        pass
 
-for portal in os.listdir(INPUT_DIR):
     for viewid_raw in open(os.path.join(INPUT_DIR, portal, 'viewids')).readlines():
         viewid = viewid_raw[:-1]
-        portal = 'explore.data.gov'
-        viewid = 'bxfh-jivs'
         url = 'http://%s/api/views/%s/rows.csv?accessType=DOWNLOAD' % (portal, viewid)
-        urlretrieve(url, filename = os.path.join(OUTPUT_DIR, viewid))
+        urlretrieve(url, filename = os.path.join(portal_dir, viewid))
         break
-    break
+
+if 'http_proxy' not in os.environ:
+    raise UserWarning('You have not set an http_proxy.')
+
+for portal in os.listdir(INPUT_DIR):
+    portal_rows(portal)
